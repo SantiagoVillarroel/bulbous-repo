f058624f2b070cda963155a6117d7022c0ee751c
Santiago <Santiago@192.168.0.241>
2020-08-30 14:28:44 -0300

    segundo-commit

--- GitPrueba/src/gitprueba/GitPrueba.java
+++ GitPrueba/src/gitprueba/GitPrueba.java
@@ -17,5 +17,8 @@ public class GitPrueba {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public static void prueba(){
+        
+    }
     
 }
