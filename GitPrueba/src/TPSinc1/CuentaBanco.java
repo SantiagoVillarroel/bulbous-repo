c3e2d7ab88d3855e91f693fb3e5e31162917cf8b
Santiago <Santiago@192.168.0.241>
2020-09-12 15:00:57 -0300

    Se agregó ejercicio 1 de TP de Sync

---
+++ GitPrueba/src/TPSinc1/CuentaBanco.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPSinc1;
+
+/**
+ *
+ * @author Santiago
+ */
+public class CuentaBanco {
+
+    private int balance = 50;
+
+    public CuentaBanco() {
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void retiroBancario(int retiro) {
+        balance = balance - retiro;
+    }
+}
