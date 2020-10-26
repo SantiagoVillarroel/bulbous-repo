d9d9354c1ee9119c0d22f4987280e36280de1d38
Santiago <Santiago@192.168.0.241>
2020-10-26 17:47:42 -0300

    Se agregó problemas de Productor - Consumidor (con Buffer limitado/ilimitado)

---
+++ GitPrueba/src/ProdCons/Producto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProdCons;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Producto {
+    private Object elem;
+    public Producto(){
+        this.elem=(int)Math.random();
+    }
+}
