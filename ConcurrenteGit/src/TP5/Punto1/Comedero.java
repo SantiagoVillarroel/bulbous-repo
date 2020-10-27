42d016b194e0999f953440d7eef1613be51f9072
Faustino <Faustino@LAPTOP-VI7AL9FL>
2020-10-27 20:28:35 -0300

    Comedor

---
+++ ConcurrenteGit/src/TP5/Punto1/Comedero.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP5.Punto1;
+
+public class Comedero {
+    private boolean ocupado;
+
+    public Comedero() {
+        this.ocupado = false;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean var) {
+        this.ocupado = var;
+    }
+    
+    
+}
---
+++ ConcurrenteGit/src/TP5/Punto1/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP5.Punto1;
+
+/**
+ *
+ * @author Faustino
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int cantgatos = 5, cantperros = 7;
+        Thread[] gatos = new Thread[cantgatos];
+        Thread[] perros = new Thread[cantperros];
+        Comedor com = new Comedor(2);
+        AdminComedor ac = new AdminComedor(com, cantgatos, cantperros);
+        int max = Math.max(cantgatos, cantperros);
+        for (int i = 0; i <= max - 1; i++) {
+            if (i <= cantgatos - 1) {
+                gatos[i] = new Thread(new Gato(ac));
+            }
+            if (i <= cantperros - 1) {
+                perros[i] = new Thread(new Perro(ac));
+            }
+        }
+
+        for (int i = 0; i <= max - 1; i++) {
+            if (i <= cantgatos - 1) {
+                gatos[i].start();
+            }
+            if (i <= cantperros - 1) {
+                perros[i].start();
+            }
+        }
+
+    }
+}
